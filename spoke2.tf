b955d6359713afaa10140a743e382414ebb66d7f
sharatthota <64753407+sharatthota@users.noreply.github.com>
2022-03-12 10:06:33 +0100

    Update

---
+++ spoke2.tf
@@ -0,0 +1,101 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAWI7TGBPHH57VH45Q"
+  secret_key = "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"
+}
+
+#vpc creation
+
+resource "aws_vpc" "spoke2vpc" {
+  cidr_block = "172.33.0.0/16"
+
+  tags = {
+    name = "spoke2vpc"
+  }
+}
+
+#Created internet gateway
+resource "aws_internet_gateway" "igspoke2" {
+  vpc_id = aws_vpc.spoke2vpc.id
+
+  tags = {
+    Name = "igspoke2"
+  }
+}
+
+#creating routetable
+resource "aws_route_table" "route-private-spoke2" {
+  vpc_id = aws_vpc.spoke2vpc.id
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igspoke2.id
+  }
+
+   route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id             = aws_internet_gateway.igspoke2.id
+  }
+ 
+  tags = {
+    Name = "route-table-spoke2"
+  }
+}
+
+# Create a pvt-subnets main1,main2,main3
+
+resource "aws_subnet" "pvt_main1" {
+  vpc_id     = aws_vpc.spoke2vpc.id
+  cidr_block = "172.33.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet1-spoke2"
+  }
+}
+
+# Create a private-subnet2
+resource "aws_subnet" "pvt_main2" {
+  vpc_id     = aws_vpc.spoke2vpc.id
+  cidr_block = "172.33.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-subnet2-spoke2"
+  }
+}
+
+# Create a private-subnet3
+resource "aws_subnet" "pvt_main3" {
+  vpc_id     = aws_vpc.spoke2vpc.id
+  cidr_block = "172.33.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "private-subnet3-spoke2"
+  }
+}
+
+
+#Associate subnet with route table
+resource "aws_route_table_association" "route-private-spoke2-main1" {
+  subnet_id      = aws_subnet.pvt_main1.id
+  route_table_id = aws_route_table.route-private-spoke2.id
+
+resource "aws_route_table_association" "route-private-spoke2-main2" {
+  subnet_id      = aws_subnet.pvt_main2.id
+  route_table_id = aws_route_table.route-private-spoke2.id
+}
+resource "aws_route_table_association" "route-private-spoke2-main3" {
+  subnet_id      = aws_subnet.pvt_main3.id
+  route_table_id = aws_route_table.route-private-spoke2.id
+}
+
+
+
+
+
+
+
+
+
