0e21accd594fcea4a21b076a4245b612e28ece0c
sharatthota <64753407+sharatthota@users.noreply.github.com>
2022-03-12 10:08:14 +0100

    Update spoke2.tf

--- spoke2.tf
+++ spoke2.tf
@@ -1,8 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAWI7TGBPHH57VH45Q"
-  secret_key = "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"
+  access_key = "*********"
+  secret_key = "*********"
 }
 
 #vpc creation
